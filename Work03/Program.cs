bef41a2f29040caaad07e513c4481d1cd4c2066a
FILIN <hagi_beter@mail.ru>
2023-04-15 00:22:46 +0300

    Work03

---
+++ Work03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для таблицы квадратов: ");
+string text1 = Console.ReadLine();
+int a = Convert.ToInt32(text1);
+
+for (int i = 1; i <= a ; i++)
+{
+    double x = Math.Pow(i, 3);
+    Console.WriteLine(x); // Вывод в консоль переменной i
+
+}
