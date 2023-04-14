ea842dcc03e4d27907620d28eaf1f192400fca46
FILIN <hagi_beter@mail.ru>
2023-04-15 00:11:14 +0300

    Work01

---
+++ Work01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Напишите пяти значное число, проверим его на палиндромность: ");
+string str = Console.ReadLine();
+int a = str[0];
+int b = str[1];
+int c = str[3];
+int d = str[4];
+
+if (a == d && b == c)
+{
+    Console.WriteLine($"Это значение {str} является палиндромным ");
+}
+else
+{
+    Console.WriteLine($"Это значение {str} не является палиндромным ");
+}
+
